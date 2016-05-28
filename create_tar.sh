2091550a84e39ddcaa6255a6563a47d6f2b2ed1f
Isuru Fernando <isuruf@gmail.com>
2016-05-28 11:39:33 +0530

    Remove create_tar.sh

--- create_tar.sh
+++
@@ -1,6 +0,0 @@
-#! /bin/bash
-
-cd ../symengine
-tar -zcvf libsymengine_0.1.0.orig.tar.gz --exclude=".*" --exclude="benchmarks" --exclude="symengine/utilities/teuchos" *
-cd ../symengine.deb
-mv ../symengine/libsymengine_0.1.0.orig.tar.gz .
