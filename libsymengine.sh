1d457bdca1fbcff47b42a85dd9fe1bb5f1c3bb68
Isuru Fernando <isuruf@gmail.com>
2016-05-28 14:21:36 +0530

    Python packages

--- libsymengine.sh
+++ libsymengine.sh
@@ -1,26 +1,27 @@
 #! /bin/bash
 
 version=0.1.0-ubuntu0
-build=0
+build=1
 
 cd ../symengine
 tar -zcvf libsymengine_${version}.orig.tar.gz --exclude=".*" --exclude="benchmarks" --exclude="symengine/utilities/teuchos" *
 cd ../symengine.deb
 mv ../symengine/libsymengine_${version}.orig.tar.gz .
 
-for dist in trusty xenial wily precise
+for dist in xenial trusty wily precise
 do
     rm -rf libsymengine-${version}
     ls -l
     cp -r libsymengine libsymengine-${version}
     cd libsymengine-${version}
-    sed -i 's:libsymengine (version-build_num) dist:libsymengine ('${version}'-'${dist}${build_num}') '${dist}':g' debian/changelog
+    sed -i 's:libsymengine (version) dist:libsymengine ('${version}'-'${dist}${build}') '${dist}':g' debian/changelog
     if [ "$dist" == "precise" ]; then
         sed -i 's/cmake,/cmake, g++-4.8,/g' debian/control
-        sed -i '6iDEB_CONFIGURE_SCRIPT_ENV += CC=gcc-4.8' debian/rules
-        sed -i '7iDEB_CONFIGURE_SCRIPT_ENV += CXX=g++-4.8' debian/rules
+        sed -i '6iDEB_CMAKE_EXTRA_FLAGS += -DCMAKE_C_COMPILER=gcc-4.8' debian/rules
+        sed -i '7iDEB_CMAKE_EXTRA_FLAGS += -DCMAKE_CXX_COMPILER=g++-4.8' debian/rules
     fi
     debuild -S
     cd ..
-    #dput ppa:isuruf/symengine libsymengine_${version}-${dist}${build}_source.changes 
+    dput ppa:isuruf/symengine libsymengine_${version}-${dist}${build}_source.changes
 done
+
---
+++ python-symengine.sh
@@ -0,0 +1,28 @@
+#! /bin/bash
+
+version=0.1.0-ubuntu0
+build=1
+
+cd ../symengine.py
+tar -zcvf python-symengine_${version}.orig.tar.gz --exclude=".*" --exclude="benchmarks" *
+cd ../symengine.deb
+mv ../symengine.py/python-symengine_${version}.orig.tar.gz .
+
+for dist in xenial trusty wily precise
+do
+    rm -rf python-symengine-${version}
+    ls -l
+    cp -r python-symengine python-symengine-${version}
+    cd python-symengine-${version}
+    sed -i 's:python-symengine (version) dist:python-symengine ('${version}'-'${dist}${build}') '${dist}':g' debian/changelog
+    if [ "$dist" == "precise" ]; then
+        TAB=$'\t'
+        sed -i 's/cmake,/cmake, g++-4.8,/g' debian/control
+        sed -i '6i\\texport CC=gcc-4.8' debian/rules
+        sed -i '7i\\texport CXX=g++-4.8' debian/rules
+    fi
+    debuild -S -sa
+    cd ..
+    dput ppa:isuruf/symengine python-symengine_${version}-${dist}${build}_source.changes 
+done
+
