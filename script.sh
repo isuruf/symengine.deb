2b7a82ae518dada2e1312d19989f1cfc002cd1fd
Isuru Fernando <isuruf@gmail.com>
2016-05-28 10:04:35 +0530

    precise

--- script.sh
+++ script.sh
@@ -1,12 +1,14 @@
+#! /bin/bash
+
 version=0.1.0
-build=1
+build=0
 
 cd ../symengine
-tar -zcvf libsymengine_$version.orig.tar.gz --exclude=".*" --exclude="benchmarks" --exclude="symengine/utilities/teuchos" *
+tar -zcvf libsymengine_${version}.orig.tar.gz --exclude=".*" --exclude="benchmarks" --exclude="symengine/utilities/teuchos" *
 cd ../symengine.deb
-mv ../symengine/libsymengine_$version.orig.tar.gz .
+mv ../symengine/libsymengine_${version}.orig.tar.gz .
 
-for dist in trusty wily xenial
+for dist in trusty xenial wily precise
 do
     rm -rf libsymengine-${version}
     ls -l
@@ -16,9 +18,10 @@ do
     sed -i 's/build_num/'${dist}${build}'/g' debian/changelog
     if [ "$dist" == "precise" ]; then
         sed -i 's/cmake,/cmake, g++-4.8,/g' debian/control
-        git apply precise.diff
+        sed -i '6iDEB_CONFIGURE_SCRIPT_ENV += CC=gcc-4.8' debian/rules
+        sed -i '7iDEB_CONFIGURE_SCRIPT_ENV += CXX=g++-4.8' debian/rules
     fi
     debuild -S
     cd ..
-    #dput ppa:isuruf/symengine libsymengine_$version-$dist$build_source.changes 
+    #dput ppa:isuruf/symengine libsymengine_${version}-${dist}${build}_source.changes 
 done
