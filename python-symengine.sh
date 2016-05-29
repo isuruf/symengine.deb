55433cdcb2fd6c4ce112207fd8f5e2fed22b85ff
Isuru Fernando <isuruf@gmail.com>
2016-05-29 23:06:18 +0530

    Fix python-symengine

--- python-symengine.sh
+++ python-symengine.sh
@@ -1,19 +1,21 @@
 #! /bin/bash
 
-version=0.1.0-ubuntu0
-build=1
+version=0.1.0-ubuntu8
+build=0
 
 cd ../symengine.py
 tar -zcvf python-symengine_${version}.orig.tar.gz --exclude=".*" --exclude="benchmarks" *
 cd ../symengine.deb
 mv ../symengine.py/python-symengine_${version}.orig.tar.gz .
 
-for dist in xenial trusty wily precise
+for dist in xenial trusty wily #precise
 do
     rm -rf python-symengine-${version}
     ls -l
+
     cp -r python-symengine python-symengine-${version}
     cd python-symengine-${version}
+    cp ../../symengine.py/setup.py setup.py
     sed -i 's:python-symengine (version) dist:python-symengine ('${version}'-'${dist}${build}') '${dist}':g' debian/changelog
     if [ "$dist" == "precise" ]; then
         TAB=$'\t'
