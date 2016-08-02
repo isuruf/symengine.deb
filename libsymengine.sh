93e3d12e7e8244dcbeaa757fd8c0b6025a08f206
Isuru Fernando <isuru.11@cse.mrt.ac.lk>
2016-08-02 18:18:45 +0530

    Remove precise

--- libsymengine.sh
+++ libsymengine.sh
@@ -8,18 +8,13 @@ tar -zcvf libsymengine_${version}.orig.tar.gz --exclude=".*" --exclude="benchmar
 cd ../symengine.deb
 mv ../symengine/libsymengine_${version}.orig.tar.gz .
 
-for dist in xenial trusty wily precise
+for dist in xenial trusty wily
 do
     rm -rf libsymengine-${version}
     ls -l
     cp -r libsymengine libsymengine-${version}
     cd libsymengine-${version}
     sed -i 's:libsymengine (version) dist:libsymengine ('${version}'-'${dist}${build}') '${dist}':g' debian/changelog
-    if [ "$dist" == "precise" ]; then
-        sed -i 's/cmake,/cmake, g++-4.8,/g' debian/control
-        sed -i '6iDEB_CMAKE_EXTRA_FLAGS += -DCMAKE_C_COMPILER=gcc-4.8' debian/rules
-        sed -i '7iDEB_CMAKE_EXTRA_FLAGS += -DCMAKE_CXX_COMPILER=g++-4.8' debian/rules
-    fi
     debuild -S
     cd ..
     dput ppa:isuruf/symengine libsymengine_${version}-${dist}${build}_source.changes
