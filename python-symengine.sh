e1ab386e5924d1045e785198cf4772de1dabbe2e
Isuru Fernando <isuruf@gmail.com>
2016-09-27 10:03:23 +0530

    update python-symengine

--- python-symengine.sh
+++ python-symengine.sh
@@ -1,8 +1,24 @@
 #! /bin/bash
 
-version=0.1.0-ubuntu8
-build=0
+build=1
 
+cd ../symengine.py
+git_ver=`git describe --tags`
+echo $git_ver
+if [[ $git_ver == "v"* ]]
+then
+  git_ver=${git_ver:1};
+fi
+
+cd ../symengine
+cpp_git_ver=`git describe --tags`
+echo $cpp_git_ver
+if [[ $cpp_git_ver == "v"* ]]
+then
+  cpp_git_ver=${cpp_git_ver:1};
+fi
+
+version=$git_ver-ubuntu0
 cd ../symengine.py
 tar -zcvf python-symengine_${version}.orig.tar.gz --exclude=".*" --exclude="benchmarks" *
 cd ../symengine.deb
@@ -11,14 +27,13 @@ mv ../symengine.py/python-symengine_${version}.orig.tar.gz .
 for dist in xenial trusty wily #precise
 do
     rm -rf python-symengine-${version}
-    ls -l
-
     cp -r python-symengine python-symengine-${version}
     cd python-symengine-${version}
     cp ../../symengine.py/setup.py setup.py
     sed -i 's:python-symengine (version) dist:python-symengine ('${version}'-'${dist}${build}') '${dist}':g' debian/changelog
+    sed -i 's:libsymengine-dev:libsymengine-dev (>='$cpp_git_ver'):g' debian/control
     debuild -S -sa
     cd ..
-    dput ppa:isuruf/symengine python-symengine_${version}-${dist}${build}_source.changes 
+    dput ppa:symengine/ppa python-symengine_${version}-${dist}${build}_source.changes
 done
 
